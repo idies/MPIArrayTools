2fc3d413b0e44b71e757cb4b46f824929f63e594
Chichi Lalescu <clalesc1@jhu.edu>
2015-02-10 16:48:24 -0500

    put class into its own file

--- makefile
+++ makefile
@@ -1,7 +1,30 @@
-transpose_2D:
-	mpic++ transp.cpp \
+MPICXX  = mpicxx
+LINKER  = mpicxx
+DEFINES =
+CFLAGS  =
+
+LIBS = -lfftw3_mpi \
+	   -lfftw3 \
+	   -lfftw3f_mpi \
+	   -lfftw3f
+
+vpath %.cpp ./src/
+
+src := \
+	transp.cpp \
+	field_descriptor.cpp
+
+obj := $(patsubst %.cpp, ./obj/%.cpp.o, ${src})
+
+./obj/%.cpp.o: %.cpp
+	${MPICXX} ${DEFINES} \
+		${CFLAGS} \
+		-c $^ -o $@
+
+transpose_2D: ${obj}
+	${LINKER} \
+		${obj} \
 		-o t2D \
-		-lfftw3_mpi \
-		-lfftw3 \
-		-lfftw3f_mpi \
-		-lfftw3f
+		${LIBS} \
+		${NULL}
+
---
+++ src/field_descriptor.hpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h>
+#include <fftw3-mpi.h>
+
+#ifndef __FIELD_DESCRIPTOR__
+
+#define __FIELD_DESCRIPTOR__
+
+extern int myrank, nprocs;
+
+/**********************************************************************/
+class field_descriptor
+{
+    public:
+        int *sizes;
+        int *subsizes;
+        int *starts;
+        int ndims;
+        int local_size, full_size;
+        MPI_Datatype mpi_array_dtype, mpi_dtype;
+
+        field_descriptor(){}
+        ~field_descriptor(){}
+        int initialize(
+                int ndims,
+                int *n,
+                MPI_Datatype element_type);
+        int finalize();
+        int read(
+                const char *fname,
+                void *buffer);
+        int write(
+                const char *fname,
+                void *buffer);
+};
+
+#endif//__FIELD_DESCRIPTOR__
+
