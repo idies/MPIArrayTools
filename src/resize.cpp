c0afab7ebadf01e730613fa3d85cd25a7af14c4d
Chichi Lalescu <clalesc1@jhu.edu>
2015-02-12 14:01:59 -0500

    actually check resize

--- src/resize.cpp
+++ src/resize.cpp
@@ -11,7 +11,8 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
     int n[3];
-    field_descriptor *f0, *f1, *f2;
+    field_descriptor *f0r, *f0c;
+    field_descriptor *f1r, *f1c;
 
     if (argc != 4)
     {
@@ -23,44 +24,61 @@ int main(int argc, char *argv[])
     n[0] = atoi(argv[1]);
     n[1] = atoi(argv[2]);
     n[2] = atoi(argv[3]);
-    f0 = new field_descriptor(3, n, MPI_COMPLEX8);
+    f0c = new field_descriptor(3, n, MPI_COMPLEX8);
+    n[0] = f0c->sizes[0];
+    n[1] = f0c->sizes[1];
+    n[2] = 2*(f0c->sizes[2]-1);
+    f0r = new field_descriptor(3, n, MPI_REAL4);
+
     n[0] = 2*atoi(argv[1]);
     n[1] = 2*atoi(argv[2]);
-    n[2] = 2*(atoi(argv[3])-1)+1;
-    f1 = new field_descriptor(3, n, MPI_COMPLEX8);
-    n[0] = f1->sizes[0];
-    n[1] = f1->sizes[1];
-    n[2] = 2*(f1->sizes[2]-1);
-    f2 = new field_descriptor(3, n, MPI_REAL4);
+    n[2] = 2*atoi(argv[3]);
+    f1c = new field_descriptor(3, n, MPI_COMPLEX8);
+    n[0] = f1c->sizes[0];
+    n[1] = f1c->sizes[1];
+    n[2] = 2*(f1c->sizes[2]-1);
+    f1r = new field_descriptor(3, n, MPI_REAL4);
 
-    fftwf_complex *a0, *a1;
-    float *a2;
-    a0 = fftwf_alloc_complex(f0->local_size);
-    a1 = fftwf_alloc_complex(f1->local_size);
-    a2 = fftwf_alloc_real(2*f1->local_size);
+    fftwf_complex *a0, *a1c;
+    float *a1r;
+    a0 = fftwf_alloc_complex(f0c->local_size);
+    a1c = fftwf_alloc_complex(f1c->local_size);
+    a1r = fftwf_alloc_real(2*f1c->local_size);
 
-    f0->read("data0", (void*)a0);
-
-    fftwf_copy_complex_array(
-            f0, a0,
-            f1, a1);
+    f0c->read("data0c", (void*)a0);
 
     fftwf_plan c2r = fftwf_mpi_plan_dft_c2r_3d(
-            f2->sizes[0], f2->sizes[1], f2->sizes[2],
-            a1, a2,
+            f0r->sizes[0], f0r->sizes[1], f0r->sizes[2],
+            a0, a1r,
             MPI_COMM_WORLD,
             FFTW_ESTIMATE);
     fftwf_execute(c2r);
     fftwf_destroy_plan(c2r);
-    fftwf_clip_zero_padding(f2, a2);
-    f2->write("data2", (void*)a2);
-    fftw_free(a0);
-    fftw_free(a1);
-    fftw_free(a2);
+    fftwf_clip_zero_padding(f0r, a1r);
+    f0r->write("data0r", (void*)a1r);
 
-    delete f0;
-    delete f1;
-    delete f2;
+    fftwf_copy_complex_array(
+            f0c, a0,
+            f1c, a1c);
+
+    c2r = fftwf_mpi_plan_dft_c2r_3d(
+            f1r->sizes[0], f1r->sizes[1], f1r->sizes[2],
+            a1c, a1r,
+            MPI_COMM_WORLD,
+            FFTW_ESTIMATE);
+    fftwf_execute(c2r);
+    fftwf_destroy_plan(c2r);
+
+    fftwf_clip_zero_padding(f1r, a1r);
+    f1r->write("data1r", (void*)a1r);
+    fftw_free(a0);
+    fftw_free(a1c);
+    fftw_free(a1r);
+
+    delete f0r;
+    delete f0c;
+    delete f1r;
+    delete f1c;
     MPI_Finalize();
     return EXIT_SUCCESS;
 }
