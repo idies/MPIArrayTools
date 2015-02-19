f19ffee89d6e5a70e656513ceb87a7519f7cf36c
Chichi Lalescu <clalesc1@jhu.edu>
2015-02-19 11:33:05 -0500

    put back test code

--- src/full.cpp
+++ src/full.cpp
@@ -10,16 +10,20 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
     RMHD_converter *bla = new RMHD_converter(
-            (1364/2+1), 1364, 1364,
-            2048, 2048, 2048,
-            64);
+            atoi(argv[1]), atoi(argv[2]), atoi(argv[3]),
+            atoi(argv[4]), atoi(argv[5]), atoi(argv[6]),
+            2);
+//            (1364/2+1), 1364, 1364,
+//            2048, 2048, 2048,
+//            64);
+    bla->convert("Kdata0", "Kdata1", "Rdata");
 
-    bla->convert("K138000QNP002",
-                 "K138000QNP003",
-                 "U138000");
-    bla->convert("K138000QNP005",
-                 "K138000QNP006",
-                 "B138000");
+    //bla->convert("K138000QNP002",
+    //             "K138000QNP003",
+    //             "U138000");
+    //bla->convert("K138000QNP005",
+    //             "K138000QNP006",
+    //             "B138000");
     delete bla;
 
     // clean up
