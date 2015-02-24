a7f6277c8df4b5ef3703ee49c53643382ccead12
Chichi Lalescu <clalesc1@jhu.edu>
2015-02-24 13:33:17 -0500

    use 3 fields in full test

--- src/full.cpp
+++ src/full.cpp
@@ -10,12 +10,13 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
-    int n, N, nfiles;
-    if (argc == 4)
+    int n, N, nfiles, nfields;
+    if (argc == 5)
     {
         n = atoi(argv[1]);
         N = atoi(argv[2]);
         nfiles = atoi(argv[3]);
+        nfields = atoi(argv[4]);
     }
     else
     {
@@ -28,11 +29,11 @@ int main(int argc, char *argv[])
     p3DFFT_to_iR *r = new p3DFFT_to_iR(
             (n/2+1), n, n,
             N, N, N,
-            2);
+            nfields);
 
     // initialize file names
-    char* ifile[2];
-    for (int i=0; i<2; i++)
+    char* ifile[nfields];
+    for (int i=0; i<nfields; i++)
     {
         ifile[i] = (char*)malloc(100*sizeof(char));
         sprintf(ifile[i], "Kdata%d", i);
@@ -40,11 +41,11 @@ int main(int argc, char *argv[])
 
     //read
     r->read(ifile);
-    for (int i = 0; i<2; i++)
+    for (int i = 0; i<nfields; i++)
         free(ifile[i]);
 
     Morton_shuffler *s = new Morton_shuffler(
-            N, N, N, 2, nfiles);
+            N, N, N, nfields, nfiles);
     s->shuffle(r->r3, "Rdata");
 
     delete s;
